7b1bb107817f7501fe20e65009b2cdbc19c27ac3
Soupon2gou <Soupon2gou@outlook.jp>
2023-07-19 00:26:44 +0900

    Initial commit

--- script.js
+++ script.js
@@ -5,16 +5,27 @@ function play(playerChoice) {
     var result = '';
 
     if (playerChoice === computerChoice) {
-        result = "どろー";
+        result = "どろー　もう一回！";
     } else if (
         (playerChoice === 'rock' && computerChoice === 'scissors') ||
         (playerChoice === 'paper' && computerChoice === 'rock') ||
         (playerChoice === 'scissors' && computerChoice === 'paper')
     ) {
-        result = "かった～";
+        result = "あなたの勝ち　；；";
     } else {
-        result = "ざ～～～～～～こwwwwwwwwwwwwww";
+        result = "あなたの負け　ざ～～～～～～こwwwwwwwwwwwwww";
     }
 
+    var resultElement = document.getElementById('result');
     document.getElementById('result').innerHTML = result;
+
+
+    // 選択された手に基づいて画像を表示
+    var playerImage = '<img src="' + playerChoice + '.png" alt="' + playerChoice + '">';
+    var computerImage = '<img src="' + computerChoice + '.png" alt="' + computerChoice + '">';
+
+    resultElement.innerHTML += '<br>' + playerImage + ' vs ' + computerImage ;
+    resultElement.innerHTML += '<br>' + '　　　　おまえ' + '　　　　　　　　　　　メスガキ';
+
+
 }
